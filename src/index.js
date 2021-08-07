c7804d92516843e0b84e3a66666996f9439ae781
DanTel <DaniyarT2005@gmail.com>
2021-08-07 23:41:25 +0600

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  return array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  return array.sort((a, b) => b - a)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) return 0;
+  let sum = array.reduce((sum, current) => sum + current, 0);
+  return sum / array.length;
 }
